5f5d1be363e988376d094f139f24c3c24f8d41ee
Michael Simons <michael.simons@neo4j.com>
2019-03-22 23:22:05 +0100

    Use Graal + Truffle instead of Script engine.

--- src/main/java/scripts/Scripts.java
+++ src/main/java/scripts/Scripts.java
@@ -4,27 +4,31 @@ import java.util.List;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import javax.script.ScriptException;
+
+import org.graalvm.polyglot.Value;
 import org.neo4j.kernel.impl.core.EmbeddedProxySPI;
 import org.neo4j.kernel.impl.core.GraphProperties;
 import org.neo4j.kernel.impl.core.GraphPropertiesProxy;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
-import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Mode;
-import org.neo4j.procedure.PerformsWrites;
+import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 import org.neo4j.procedure.UserFunction;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-
-import static org.neo4j.helpers.collection.MapUtil.stringMap;
-
 public class Scripts {
+
+    enum Language {
+        JAVASCRIPT("js");
+
+        public final String id;
+
+        Language(String id) {
+            this.id = id;
+        }
+    }
 
     private static String PREFIX = "script.function.";
     public static final Object[] NO_OBJECTS = new Object[0];
@@ -38,49 +42,66 @@ public class Scripts {
     private static final GraphPropertiesProxy NO_GRAPH_PROPERTIES = new GraphPropertiesProxy(null);
     private static GraphProperties graphProperties = NO_GRAPH_PROPERTIES;
 
-    private static ThreadLocal<ScriptEngine> engine = new ThreadLocal<>();
+    static org.graalvm.polyglot.Context polyglotContext() {
+        org.graalvm.polyglot.Context context = org.graalvm.polyglot.Context
+            .newBuilder().allowAllAccess(true).build();
+
+        Value helperFunctions = context.getBindings(Language.JAVASCRIPT.id);
+        helperFunctions
+            .putMember("label", context.eval(Language.JAVASCRIPT.id, "s => org.neo4j.graphdb.Label.label(s)"));
+        helperFunctions.putMember("type",
+            context.eval(Language.JAVASCRIPT.id, "s => org.neo4j.graphdb.RelationshipType.withName(s)"));
+        helperFunctions.putMember("collection", context.eval(Language.JAVASCRIPT.id,
+            "it => { r=[]; while (it.hasNext()) r.push(it.next());  return Java.to(r); }"));
+        return context;
+    }
 
     private GraphProperties graphProperties() {
         if (graphProperties == NO_GRAPH_PROPERTIES)
             graphProperties = this.db.getDependencyResolver().resolveDependency(EmbeddedProxySPI.class).newGraphPropertiesProxy();
         return graphProperties;
     }
-    private ScriptEngine getEngine() {
-        if (engine.get() == null) {
-            ScriptEngine js = new ScriptEngineManager().getEngineByName("nashorn");
-            addFunctions(js,
-                    "function label(s) org.neo4j.graphdb.Label.label(s)",
-                    "function type(s) org.neo4j.graphdb.RelationshipType.withName(s)",
-                    "function collection(it) { r=[]; while (it.hasNext()) r.push(it.next());  return Java.to(r); }");
-            engine.set(js);
-        }
-        return engine.get();
-    }
-
-    private void addFunctions(ScriptEngine js, String...script) {
-        try {
-            for (String s : script) js.eval(s);
-        } catch (ScriptException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @UserFunction("scripts.run")
-    public Object runFunction(@Name("name") String name, @Name(value="params",defaultValue="[]") List<Object> params) throws ScriptException, NoSuchMethodException {
-        ScriptEngine js = getEngine();
-        String code = (String) graphProperties().getProperty(PREFIX + name, null);
-        if (code == null)
-            throw new RuntimeException("Function " + name + " not defined, use CALL function('name','code') ");
+    public Object runFunction(@Name("name") String name, @Name(value="params",defaultValue="[]") List<Object> params)  {
+        try (org.graalvm.polyglot.Context context = polyglotContext()) {
+            String code = (String) graphProperties().getProperty(PREFIX + name, null);
+            if (code == null)
+                throw new RuntimeException("Function " + name + " not defined, use CALL function('name','code') ");
 
-        js.put("db", db);
-        js.put("log", log);
-        js.eval(String.format("function %s(){ return (%s).apply(this, arguments) }", name, code));
-        return ((Invocable) js).invokeFunction(name, params == null ? NO_OBJECTS : params.toArray());
+            Value bindings = context.getPolyglotBindings();
+            bindings.putMember("db", db);
+            bindings.putMember("log", log);
+            bindings.putMember("arguments", params == null ? NO_OBJECTS : params.toArray());
+
+            Value result = context.eval(Language.JAVASCRIPT.id,
+                String.format("() => {"
+                    + "  var db = Polyglot.import(\"db\"); "
+                    + "  var log = Polyglot.import(\"log\"); "
+                    + "  return (%s).apply(this, Polyglot.import(\"arguments\"));"
+                    + "}", code))
+                .execute();
+
+            if (result.isNull()) {
+                return null;
+            }
+            if (result.isHostObject()) {
+                return result.asHostObject();
+            }
+            if (result.isNumber()) {
+                return result.asDouble();
+            }
+            if (result.isBoolean()) {
+                return result.asBoolean();
+            }
+
+            return result.asString();
+        }
     }
 
     @Procedure
     public Stream<Result> run(@Name("name") String name, @Name(value="params",defaultValue="[]") List<Object> params) throws ScriptException, NoSuchMethodException {
-        Object value = runFunction(name, params); 
+        Object value = runFunction(name, params);
         if (value instanceof Object[]) {
              return Stream.of((Object[]) value).map(Result::new);
          }
@@ -92,8 +113,9 @@ public class Scripts {
 
     @Procedure(mode=Mode.WRITE)
     public Stream<Result> function(@Name("name") String name, @Name("code") String code) throws ScriptException {
-        ScriptEngine js = getEngine();
-        js.eval("function(){" + code + "}");
+        try (org.graalvm.polyglot.Context context = polyglotContext()) {
+            context.eval(Language.JAVASCRIPT.id, "var tmp = " + code);
+        }
         GraphProperties props = graphProperties();
         boolean replaced = props.hasProperty(PREFIX + name);
         props.setProperty(PREFIX + name, code);
